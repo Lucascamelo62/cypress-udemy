fe940faf8d207b05bd7d66745d3fa0a6be4f19dd
Lucas Camelo <lucascamelo@MacBook-Air-de-Lucas.local>
2020-08-11 22:20:23 -0300

    Äula 1 - Arrow Functio

---
+++ cypress/integration/examples/arrow.spec.js
@@ -0,0 +1,20 @@
+it ('Primeira funcao de teste')
+
+//Declarando as variaveis
+const a = 10
+const b = 20
+
+//Criando a funcao com function
+function somaFunction (a,b){
+    return a + b;
+}
+//Imprimindo as variaveis
+console.log(a+b)
+
+//Criando com arrow function
+const somaArrow = (a, b) => {
+    a + b
+}
+//Imprimindo as variaveis
+console.log(a+b)
+
